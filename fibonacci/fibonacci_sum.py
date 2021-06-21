ead2b6b3fe53381b3ad65442ddb133a0bb175cec
asim <asimalilatif@gmail.com>
2021-06-21 23:24:04 +0500

    execution timer

--- fibonacci/fibonacci_sum.py
+++ fibonacci/fibonacci_sum.py
@@ -5,11 +5,20 @@ def fib_rec_basic(n):
   return fib_rec_basic(n-1) + fib_rec_basic(n-2)
 
 def fib_rec_tailcall(n):
+  "Recursion with tailcall optimization."
   def calc_fib(n, a, b):
     if n == 0: return a
     return calc_fib(n-1, b, a+b)
   return calc_fib(n, 0 ,1)
 
+import time
+t = time.time()
+fib_rec_tailcall(40)
+f = time.time()
+print(f"Time was {f-t}")
 
-print(fib_rec_basic(20))
-print(fib_rec_tailcall(20))
+s = time.time()
+print(fib_rec_basic(40))
+e = time.time()
+print(f"Time was {e-s}")
+
