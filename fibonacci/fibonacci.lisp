6f99f204498ae2be84cb9c53361d59e0d5677a58
asim <asimalilatif@gmail.com>
2021-06-21 14:28:17 +0500

    reindent

--- fibonacci/fibonacci.lisp
+++ fibonacci/fibonacci.lisp
@@ -1,17 +1,17 @@
 (defun bogo-fib (n)
-	"A bogus implementation of the fibonacci function."
-	(if (< n 2)
-		n
-		(+ (bogo-fib (- n 1))
-			 (bogo-fib (- n 2)))))
+  "A bogus implementation of the fibonacci function."
+  (if (< n 2)
+    n
+    (+ (bogo-fib (- n 1))
+       (bogo-fib (- n 2)))))
 
 (defun tail-recursive-fib (n)
-	"Tail recursiveness substantially reduces execution time"
-	(labels ((calc-fib (n a b)
-										 (if (= n 0)
-											 a
-                       (calc-fib (- n 1) b (+ a b)))))
-		(calc-fib n 0 1)))
+  "Tail recursiveness substantially reduces execution time"
+  (labels ((calc-fib (n a b)
+             (if (= n 0)
+                 a
+                 (calc-fib (- n 1) b (+ a b)))))
+    (calc-fib n 0 1)))
 
 (time (print (tail-recursive-fib 40)))
 (time (print (bogo-fib 40)))
