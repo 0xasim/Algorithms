f241616f7690962a1c3358d67bcb374aeac8beeb
asim <asimalilatif@gmail.com>
2021-06-22 00:06:01 +0500

    trying to memoize in python

--- fibonacci/fibonacci_sum.py
+++ fibonacci/fibonacci_sum.py
@@ -1,4 +1,4 @@
-#Return the sum of fibonacci sequence upto n
+# Return the sum of fibonacci sequence upto n
 
 def fib_rec_basic(n):
   "Exponential? Naive recursion."
@@ -13,6 +13,17 @@ def fib_rec_tailcall(n):
     return calc_fib(n-1, b, a+b)
   return calc_fib(n, 0 ,1)
 
+from functools import lru_cache
+from memoization import cached
+#@cached(thread_safe=False)
+@lru_cache()
+def fib_rec_memo(n):
+  "O(log n). Same as naive recursion but with memoization optimization applied."
+  if n < 2:
+    return n
+  return fib_rec_basic(n-1) + fib_rec_basic(n-2)
+  
+
 def timeMe(f, n):
   import time
   s = time.time()
@@ -23,5 +34,7 @@ def timeMe(f, n):
 
 if __name__ == "__main__":
   import time
-  timeMe(fib_rec_tailcall, 403)
-  timeMe(fib_rec_basic, 30)
+  n = 30
+  timeMe(fib_rec_tailcall, n)
+  timeMe(fib_rec_memo, n)
+  timeMe(fib_rec_basic, n)
