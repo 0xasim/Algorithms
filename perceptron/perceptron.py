f41ddd4d7bf124be44124fac3caa0d8ea708d286
asim <asimalilatif@gmail.com>
2021-06-24 13:40:09 +0500

    a not working perceptron

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -6,8 +6,19 @@ class Perceptron:
     self.n_iter = n_iter
   
   def fit(self, X, y):
-    self.w_ = numpy.random.rand(x.shape[1]+1)
-    self.errors = []
+    self.w_ = np.random.rand(X.shape[1]+1)
+    self.errors_ = []
 
+    for _ in range(n_iter):
+      errors = 0
+      for xi, yi in zip(X, y):
+        update = (yi - self.predict(xi))*eta
+        w_ = w_ + update
+
+  def net_input(self, xi):
+    return np.dot(self.w_[1:], xi) + self.w_[0]
+
+  def predict(self, xi):
+    return 1 if self.net_input(xi) > 0 else -1
 if __name__ == "__main__":
   p = Perceptron()
